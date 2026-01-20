38c8c519a63a88492db14c55753cac0e19fa8385
jooyeen <pgvdzyjvn5@privaterelay.appleid.com>
2026-01-19 19:08:41 -0800

    Quartz sync: Jan 19, 2026, 7:08 PM

--- quartz/plugins/emitters/static.ts
+++ quartz/plugins/emitters/static.ts
@@ -7,16 +7,31 @@ import { dirname } from "path"
 export const Static: QuartzEmitterPlugin = () => ({
   name: "Static",
   async *emit({ argv, cfg }) {
-    const staticPath = joinSegments(QUARTZ, "static")
-    const fps = await glob("**", staticPath, cfg.configuration.ignorePatterns)
     const outputStaticPath = joinSegments(argv.output, "static")
     await fs.promises.mkdir(outputStaticPath, { recursive: true })
-    for (const fp of fps) {
-      const src = joinSegments(staticPath, fp) as FilePath
+
+    // Copy from quartz/static
+    const quartzStaticPath = joinSegments(QUARTZ, "static")
+    const quartzFps = await glob("**", quartzStaticPath, cfg.configuration.ignorePatterns)
+    for (const fp of quartzFps) {
+      const src = joinSegments(quartzStaticPath, fp) as FilePath
       const dest = joinSegments(outputStaticPath, fp) as FilePath
       await fs.promises.mkdir(dirname(dest), { recursive: true })
       await fs.promises.copyFile(src, dest)
       yield dest
+    }
+
+    // Copy from root static folder
+    const rootStaticPath = "static"
+    if (fs.existsSync(rootStaticPath)) {
+      const rootFps = await glob("**", rootStaticPath, cfg.configuration.ignorePatterns)
+      for (const fp of rootFps) {
+        const src = joinSegments(rootStaticPath, fp) as FilePath
+        const dest = joinSegments(outputStaticPath, fp) as FilePath
+        await fs.promises.mkdir(dirname(dest), { recursive: true })
+        await fs.promises.copyFile(src, dest)
+        yield dest
+      }
     }
   },
   async *partialEmit() {},
